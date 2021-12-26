caf2e3b72958cbe2d63c023fb3c09eb96a9f5ae5
nicholas-calvin <54830012+nicholas-calvin@users.noreply.github.com>
2021-12-26 13:22:16 +0700

    Merge

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,5 @@
 @extends('master')
 
-@section('header')
-
 @section('content')
 
 @endsection
-
-@section('footer')
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +24,9 @@ Route::get('/home', function () {
     return view('home');
 });
 
-Route::get('login', function () {
-    return view('login');
-});
+Route::get('/login', [LoginController::class, 'login'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
+
+Route::get('/register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store']);
