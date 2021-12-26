34655d764eb1fd1f1b24215c542a0eea7b3b7a24
nicholas-calvin <54830012+nicholas-calvin@users.noreply.github.com>
2021-12-26 13:03:36 +0700

    Login & register

---
+++ SeneKuy-main/SeneKuy/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::redirect('/', '/home');
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/login', [LoginController::class, 'login'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
+
+Route::get('/register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store']);
