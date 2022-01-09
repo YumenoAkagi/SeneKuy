ae9728bbe6cb6310d9d118cf001e5dcef1255de7
YumenoAkagi <68376091+YumenoAkagi@users.noreply.github.com>
2022-01-09 20:13:47 +0700

    login register fix

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
@@ -18,20 +20,33 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
+        $validator = Validator::make($request->all(), [
             'firstName' => 'required',
             'lastName' => 'required',
             'phoneNumber' => 'required',
             'email' => 'required|email:dns',
             'password' => 'required|confirmed|min:8',
-            'password_confirmation' => 'required'
+            'password_confirmation' => 'required|same:password'
         ]);
 
+        if($validator->fails()) {
+            return back()->with('error', 'Please fill the form correctly')->withErrors($validator);
+        }
+
+        // create new customer
+        $new_user = new User;
+        $new_user->firstName = $request->firstName;
+        $new_user->lastName = $request->lastName;
+        $new_user->phoneNumber = $request->phoneNumber;
+        $new_user->email = $request->email;
+        $new_user->password = Hash::make($request->password);
         
 
-        $validatedData['password'] = bcrypt($validatedData['password']);
-        // dd($validatedData, $cust_role_id);
-        User::create($validatedData);
+        $cust_role_id = Role::firstWhere('name', '=', 'Customer')->id;
+        $new_user->role_id = $cust_role_id;
+        
+        $new_user->save();
+        
         return redirect('/login')->with('success', 'Registration success!');
     }
 }
