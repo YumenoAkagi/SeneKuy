32c0281b8b01173c0c3643c0fea89051c7152469
YumenoAkagi <68376091+YumenoAkagi@users.noreply.github.com>
2022-01-12 21:19:24 +0700

    Route middleware

--- routes/web.php
+++ routes/web.php
@@ -20,54 +20,51 @@ use App\Http\Controllers\UsersController;
 |
 */
 
+
+// ALL USER ACCESS
 Route::redirect('/', '/home');
-
 Route::get('/home', [HomeController::class, 'showHome'])->name('home.list');
-
 Route::get('/category/{categoryId}', [HomeController::class, 'showProductCategory'])->name('category');
-// Route::get('/category/{category_id}', [UserController::class , 'showProductCategory']);
-
-Route::get('/transaction/{product_id}', [HomeController::class, 'showTransaction']);
-// Route::get('/transaction/{product_id}', [UserController::class, 'showTransaction']);
-Route::get('/category/{category_id}', [HomeController::class , 'showProductCategory']);
-
-Route::get('/transaction/{product_id}', [HomeController::class, 'showTransaction']);
-
-Route::get('/cart', [HomeController::class, 'cartList'])->name('cart.list');
-Route::post('/cart', [HomeController::class, 'addToCart'])->name('cart.store');
-Route::post('/update-cart', [HomeController::class, 'updateCart'])->name('cart.update');
-Route::post('/remove', [HomeController::class, 'removeCart'])->name('cart.remove');
-Route::post('/clear', [HomeController::class, 'clearAllCart'])->name('cart.clear');
-
-// Route::get('/cart', [UserController::class, 'cartList'])->name('cart.list');
-// Route::post('/cart', [UserController::class, 'addToCart'])->name('cart.store');
-// Route::post('/update-cart', [UserController::class, 'updateCart'])->name('cart.update');
-// Route::post('/remove', [UserController::class, 'removeCart'])->name('cart.remove');
-// Route::post('/clear', [UserController::class, 'clearAllCart'])->name('cart.clear');
-
-Route::get('/login', [LoginController::class, 'login'])->name('login')->middleware('guest');
-Route::post('/login', [LoginController::class, 'authenticate']);
-Route::post('/logout', [LoginController::class, 'logout']);
-
-Route::get('/register', [RegisterController::class, 'create'])->middleware('guest');
-Route::post('/register', [RegisterController::class, 'store']);
-
-Route::get('/product/details/{productId}', [ProductDetailController::class, 'getProductDetails'])->name('productDetail');
-
 Route::get('/aboutus', [HomeController::class, 'showCategoryAboutus'])->name('aboutus');
 
-Route::get('/shoppingcart', [HomeController::class, 'showCategoryShoppingCart'])->name('shoppingcart');
+// GUEST ONLY
+Route::middleware('guest')->group(function() {
+    Route::get('/register', [RegisterController::class, 'create'])->middleware('guest');
+    Route::post('/register', [RegisterController::class, 'store']);
+    Route::get('/login', [LoginController::class, 'login'])->name('login')->middleware('guest');
+    Route::post('/login', [LoginController::class, 'authenticate']);
+});
 
-Route::get('/profile', [HomeController::class, 'showCategoryProfile'])->name('profile');
+Route::middleware('auth')->group(function () {
+    // LOGGED IN ONLY
+    Route::post('/logout', [LoginController::class, 'logout']);
+    Route::get('/profile', [HomeController::class, 'showCategoryProfile'])->name('profile');
 
-Route::get('/wishlist', [HomeController::class, 'showCategoryWishlist'])->name('wishlist');
+    Route::get('/category/{category_id}', [HomeController::class , 'showProductCategory']);
+    Route::get('/product/details/{productId}', [ProductDetailController::class, 'getProductDetails'])->name('productDetail');
 
-Route::get('/transaction-history', [HomeController::class, 'showCategorytransactionHistory'])->name('transaction-history');
 
-Route::get('/checkout', [HomeController::class, 'showCategoryCheckout'])->name('checkout');
+    // CUSTOMER SECTION HERE
+    Route::middleware('customer')->group(function () {
+        Route::get('/cart', [HomeController::class, 'cartList'])->name('cart.list');
+        Route::post('/cart', [HomeController::class, 'addToCart'])->name('cart.store');
+        Route::post('/update-cart', [HomeController::class, 'updateCart'])->name('cart.update');
+        Route::post('/remove', [HomeController::class, 'removeCart'])->name('cart.remove');
+        Route::post('/clear', [HomeController::class, 'clearAllCart'])->name('cart.clear');
 
-Route::get('/admin', [HomeController::class, 'showCategoryAdmin'])->name('admin');
+        Route::get('/transaction/{product_id}', [HomeController::class, 'showTransaction']);
+        Route::get('/transaction-history', [HomeController::class, 'showCategorytransactionHistory'])->name('transaction-history');
 
-Route::get('/add-product', [HomeController::class, 'showCategoryAdd'])->name('addProduct');
+        Route::get('/shoppingcart', [HomeController::class, 'showCategoryShoppingCart'])->name('shoppingcart');
+        Route::get('/wishlist', [HomeController::class, 'showCategoryWishlist'])->name('wishlist');
+        Route::get('/checkout', [HomeController::class, 'showCategoryCheckout'])->name('checkout');
+    });
 
-Route::get('/delete-product', [HomeController::class, 'showCategoryDelete'])->name('deleteProduct');
+
+    // ADMIN SECTION HERE
+    Route::middleware('admin')->group(function() {
+        Route::get('/admin', [HomeController::class, 'showCategoryAdmin'])->name('admin');
+        Route::get('/add-product', [HomeController::class, 'showCategoryAdd'])->name('addProduct');
+        Route::get('/delete-product', [HomeController::class, 'showCategoryDelete'])->name('deleteProduct');
+    });
+});
