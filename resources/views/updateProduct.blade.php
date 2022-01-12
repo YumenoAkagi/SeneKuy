34f50eeb389f33e0ee163859f0e32dc3db8bd799
vjjsn <83721603+vjjsn@users.noreply.github.com>
2022-01-12 21:30:05 +0700

    update detail view

--- resources/views/updateProduct.blade.php
+++ resources/views/updateProduct.blade.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Delete Product</title>
+    <title>Update Product</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body class="p-5">
-    <h1 class="mb-5 text-center">Delete Product</h1>
+    <h1 class="mb-5 text-center">Update Product</h1>
 
     @for ($i = 0; $i < 10; $i++) <div class="card mb-2">
         <div class="card-body d-flex align-items-center">
