dd33d71b30e61b2513dea807e7f17a230a7cd2d7
nicholas-calvin <54830012+nicholas-calvin@users.noreply.github.com>
2022-01-13 08:00:10 +0700

    Update UsersSeeder.php
    password admin hashed

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsersSeeder extends Seeder
 {
@@ -14,12 +15,13 @@ class UsersSeeder extends Seeder
      */
     public function run()
     {
+        $password = Hash::make('admin');
         DB::table('users')->insert([
             'firstName' => 'Admin',
             'lastname' => 'Admin', 
             'phoneNumber' => '0000000000',
             'email' => 'admin@admin.com',
-            'password' => 'admin',
+            'password' => $password,
             'role_id' => '1'
         ]);
         // \App\Models\User::factory(10)->create();
